d3c1c09fc962908822249576890a31fbf7564f25
guykatch <guykatch@microsoft.com>
2020-02-02 11:25:03 +0200

    add acordion

--- src/components/progressDisplay.tsx
+++ src/components/progressDisplay.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion'
 import { TestResults } from './WordsTest';
 
 export interface ProgressDisplayProps{
@@ -19,11 +20,11 @@ export class ProgressDisplay extends React.Component<ProgressDisplayProps,any> {
         const testResultsRendered = 
             this.props && 
             this.props.previousTestResults &&
-            this.props.previousTestResults.map(testResult => this.getRenderedTestResult(testResult));
+            this.props.previousTestResults.map((testResult, index) => this.getRenderedTestResult2(testResult, index));
         return (
             <div>
                 <h2>
-                    Hello progress
+                    תרגולים קודמים
                 </h2>
                 <div>
                     {testResultsRendered}
@@ -49,6 +50,35 @@ export class ProgressDisplay extends React.Component<ProgressDisplayProps,any> {
             <ul>
                 TODO
             </ul>
+        </div>
+        )
+    }
+
+    getRenderedTestResult2 = (testResult: TestResults, index: number) => {
+        const dateFormatOptions = {
+            hour12 : true,
+            hour:  "2-digit",
+            minute: "2-digit",
+        };
+        return(
+        <div key={testResult.practiceDate.toString()}>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Toggle as={Button} variant="link" eventKey={index.toString()}>
+                    {new Date(testResult.practiceDate).toLocaleDateString("en-us", dateFormatOptions)}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={index.toString()}>
+                    <div>
+                        <h3>מילים ללא טעות</h3>
+                        <span>
+                            {testResult.wordsWithCorrectAnswers.join(",")}
+                        </span>
+                        <h3>מילים שטעית בהן</h3>
+                        <ul>
+                            TODO
+                        </ul>
+                    </div>                            
+                </Accordion.Collapse>  
+            </Accordion>                      
         </div>
         )
     }
