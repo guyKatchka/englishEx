9e35908c70eb3509e25efc5d227b01651d602512
guykatch <guykatch@microsoft.com>
2020-02-03 10:59:05 +0200

    commit package json

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { WordsPractice } from './components/WordsPractice'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@progress/kendo-theme-default/dist/all.css';
 
 
 const App: React.FC = () => {
